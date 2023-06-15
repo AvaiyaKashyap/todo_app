86cbb34b8433933bdbdda0ae2a97579aa59319b1
AvaiyaKashyap <avaiya.kashyap@gmail.com>
2023-06-15 12:02:30 +0530

    Slidable UI

--- lib/widgets/todo_action.dart
+++ lib/widgets/todo_action.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import 'package:flutter_slidable/flutter_slidable.dart';
 import '../providers/todo_proider.dart';
 
 
@@ -10,20 +10,37 @@ class TodoAction extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final task = Provider.of<TodoProvider>(context);
-
     return ListView.builder(
       itemCount: task.allTasks.length,
-      itemBuilder: ((context, index) => ListTile(
-        leading: Checkbox(
-          value: task.allTasks[index].completed,
-          onChanged: ((_) => task.toggleTask(task.allTasks[index])),
+      itemBuilder: ((context, index) => Slidable(
+        startActionPane: ActionPane(motion: BehindMotion(), children: [
+          SlidableAction(onPressed: (context){
+            task.deleteTask(task.allTasks[index]);
+          },
+            backgroundColor: Colors.red,
+            icon: Icons.delete,
+            label: "Delete",
+          ),
+          SlidableAction(onPressed: (context){
+          },
+            backgroundColor: Colors.green,
+            icon: Icons.check,
+            label: "Done",
+          ),
+
+        ]),
+        child: ListTile(
+          leading: Checkbox(
+            value: task.allTasks[index].completed,
+            onChanged: ((_) => task.toggleTask(task.allTasks[index])),
+          ),
+          title: Text(task.allTasks[index].todoTitle),
+          trailing: IconButton(
+              onPressed: () {
+                task.deleteTask(task.allTasks[index]);
+              },
+              icon: const Icon(Icons.delete)),
         ),
-        title: Text(task.allTasks[index].todoTitle),
-        trailing: IconButton(
-            onPressed: () {
-              task.deleteTask(task.allTasks[index]);
-            },
-            icon: const Icon(Icons.delete)),
       )),
     );
   }
